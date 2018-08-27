21d882b289a5a8e35c16db7a159a8017e3a68278
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2018-08-27 16:48:32 +0200

    fixing template

--- generate-wallpaper.js
+++ generate-wallpaper.js
@@ -2,6 +2,7 @@ const url = 'https://www.nationalgeographic.com/photography/photo-of-the-day/'
 const fs = require('fs')
 
 var page = require('webpage').create()
+page.viewportSize = { width: 1920, height: 1080 }
 page.onError = function(msg, trace) {}
 page.open(url, function() {
     setTimeout(function() {
@@ -15,16 +16,22 @@ page.open(url, function() {
 		var photoText = page.evaluate(function(){
 			return document.getElementsByTagName("p")[0].textContent
 		})
-
-		var template = fs.read('./template.html')
-		template = template.replace('IMAGE_URL', imageUrl)
-			.replace('PHOTO_TITLE', photoTitle)
-			.replace('PHOTO_TEXT', photoText)
-		page.viewportSize = { width: 1920, height: 1080 }
-		page.setContent(template, url)
-		page.onLoadFinished = function(status){
-			page.render('background.png')
+		
+		if (!imageUrl) {
 			phantom.exit()
+		} else {
+			var template = fs.read('./template.html')
+			template = template.replace('IMAGE_URL', imageUrl)
+				.replace('PHOTO_TITLE', photoTitle)
+				.replace('PHOTO_TEXT', photoText)
+			fs.write('./template.out.html', template)
+			page.viewportSize = { width: 1920, height: 1080 }
+			page.setContent(template, url)
+			page.onLoadFinished = function(status){
+				page.render('background.png')
+				phantom.exit()
+			}
 		}
+
     }, 500);
 });
