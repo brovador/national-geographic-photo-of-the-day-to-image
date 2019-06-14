bf696d505f57dd23fd7c39a049cf5dfabdd14a54
Christoph Müller <sguter90@gmail.com>
2019-06-14 08:10:55 +0200

    fixed fetching data, load data from json
    latest website change breaks current version - this change loads metadata directly from config json

--- generate-wallpaper.js
+++ generate-wallpaper.js
@@ -1,49 +1,30 @@
-const url = 'https://www.nationalgeographic.com/photography/photo-of-the-day/'
-const fs = require('fs')
+const fs = require('fs');
 
-var page = require('webpage').create()
-page.viewportSize = { width: 1920, height: 1080 }
-page.onError = function(msg, trace) {}
-page.open(url, function() {
-    setTimeout(function() {
-		
-		page.evaluate(function(){
-			var showMore = document.querySelector(".show-more-button")
-			if (showMore) {
-				showMore.click()
-			}
-		})
+var page = require('webpage').create();
+page.viewportSize = {
+    width: 2048,
+    height: 1152
+};
+page.onError = function (msg, trace) {
+    console.error(msg);
+};
 
-		var imageUrl = page.evaluate(function(){
-			var images = document.getElementsByTagName("source")[0].getAttribute("srcset").split(" ")
-			return images[images.length - 2]
-		})
-		var photoTitle = page.evaluate(function(){
-			return document.getElementsByClassName("media__caption--title")[0].textContent
-		})
-		var photoText = page.evaluate(function(){
-			var p = document.getElementsByTagName("p")[0]
-			if (p.getElementsByTagName("b").length) {
-				p.getElementsByTagName("b")[0].remove()
-			}
-			return p.textContent
-		})
-		
-		if (!imageUrl) {
-			phantom.exit()
-		} else {
-			var template = fs.read('./template.html')
-			template = template.replace('IMAGE_URL', imageUrl)
-				.replace('PHOTO_TITLE', photoTitle)
-				.replace('PHOTO_TEXT', photoText)
-			fs.write('./template.out.html', template)
-			page.viewportSize = { width: 1920, height: 1080 }
-			page.setContent(template, url)
-			page.onLoadFinished = function(status){
-				page.render('background.png')
-				phantom.exit()
-			}
-		}
+page.open('https://www.nationalgeographic.com/photography/photo-of-the-day/_jcr_content/.gallery.json', function () {
+    var result = JSON.parse(page.plainText);
+    var galleryImage = result.items[0];
 
-    }, 1500);
+    var imageUrl = galleryImage.sizes[2048];
+    var photoTitle = galleryImage.title;
+    var photoText = galleryImage.caption;
+
+    var template = fs.read('./template.html');
+    template = template.replace('IMAGE_URL', imageUrl)
+        .replace('PHOTO_TITLE', photoTitle)
+        .replace('PHOTO_TEXT', photoText);
+    fs.write('./template.out.html', template);
+    page.setContent(template, imageUrl);
+    page.onLoadFinished = function (status) {
+        page.render('background.png');
+        phantom.exit()
+    };
 });
